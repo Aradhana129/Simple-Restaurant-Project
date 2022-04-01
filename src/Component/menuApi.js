e43d3b9bf521c1fb4958efdc06ed6cf4f2bf0239
Aradhana129 <98118477+Aradhana129@users.noreply.github.com>
2022-04-02 00:23:10 +0530

    Add files via upload

---
+++ src/Component/menuApi.js
@@ -0,0 +1,82 @@
+const Menu = [
+    {
+      id: 1,
+      image: "https://st.depositphotos.com/1005893/4244/i/600/depositphotos_42443145-stock-photo-malaysian-cuisine-maggi-goreng-mamak.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "https://www.whiskaffair.com/wp-content/uploads/2020/07/Aloo-Pakora-2-1-1200x1800.jpg",
+      name: "allu pakoida",
+      category: "evening",
+      price: "20₹",
+      description:
+" Punjabi style aloo pakora. Moist and soft on inside and crisp outside is aloo pakora. There was a popular vendor who made the tastiest pakoras . My colleagues and I would often visit her during our lunch breaks to buy hot mouthwatering pakoras sandwiched in Pav(Indian buns),"    
+},
+    {
+      id: 3,
+      image: "https://akm-img-a-in.tosshub.com/sites/dailyo/fb_feed_images/story_image/201605/bhutta-small_0713150_053016095953.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+"The easiest way to enjoy fresh corn at the breakfast table is by tossing some into your scrambled eggs. Once you try this recipe, we’ll bet you’ll be saying “Why didn’t I think of that?” 4. Crustless Quiche with Summer Vegetables Add whatever summer veggies you have on hand to this recipe."
+    },
+    {
+      id: 4,
+      image: "https://media.istockphoto.com/photos/spicy-chick-peas-curry-or-chola-masala-or-chana-masala-or-chole-or-picture-id669502604?k=20&m=669502604&s=612x612&w=0&h=Rsdm21N4T3D3wjUF1j96aDZ9Y4y_24air6SFKNhdLYQ=",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+"chole is a very popular roadside chole chaat which is also called Punjabi chana masala. Who can resist the masaledar taste and tempting aroma of Chole?"  },
+    {
+      id: 5,
+      image: "https://thumbs.dreamstime.com/b/food-photography-art-pizza-menu-concept-food-photography-art-pizza-recipe-restaurant-menu-concept-102819708.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+"Image result for pizza pizzaIn the USA, Little Caesars owns the trademark Pizza! Pizza! Yet in Canada, there is another restaurant chain called Pizza Pizza which opened in 1967 in Toronto. Despite this coincidence, the two companies aren't related."    },
+    {
+      id: 6,
+      image: "https://thumbs.dreamstime.com/b/indian-thali-26440151.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+
+"Thali the meal refers to many different dishes (both vegetarian and non-vegetarian), served in small bowls called (Katori in Hindi) arranged on a Thali, or a platter. A flatbread, rice, pickle, salad, and dessert are also included, as is bread (like chapatis)."    },
+    {
+      id: 7,
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Indian_Sweet_Dessert_Peda_in_a_white_bone_china_plate.jpg/1200px-Indian_Sweet_Dessert_Peda_in_a_white_bone_china_plate.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+"Sweetness is a basic taste most commonly perceived when eating foods rich in sugars. Sweet tastes are generally regarded as pleasurable. In addition to sugars like sucrose, many other chemical compounds are sweet, including aldehydes, ketones, and sugar alcohols."    },
+    {
+      id: 8,
+      image: "https://st2.depositphotos.com/5653638/11490/i/950/depositphotos_114901612-stock-photo-cooked-red-kidney-beans-curry.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+"Rājmā, also known as rajmah, rāzmā, or lal lobia, is a vegetarian dish, originating from the Indian subcontinent, consisting of red kidney beans in a thick gravy with many Indian whole spices, and is usually served with rice. It is a part of regular diet in India,"    },
+    {
+      id: 9,
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrcQ9xAdeBaShv2rI-mazVX-mrm3FDCYVF6Pa0h_lEULcrNEWc4V96NkbI-SYoSUofObI&usqp=CAU",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+"Samosas are great because of the combination of the crispy outside, which forms during frying, and the flavourful filling on the inside. There are a lot of different samosa fillings out there and really, you can make whatever you like."    },
+  ];
+  
+  export default Menu;
+  
